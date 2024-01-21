b38a938fa6f0190d65acfc4bdf9d35c7a15953db
Nicola Coretti <nico.coretti@gmail.com>
2024-01-21 14:48:12 +0100

    Remove lua mappings

--- .config/nvim/lua/mappings/init.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = ","
-return { 
-	require("mappings/normal-mode"),
-	require("mappings/insert-mode"),
-	require("mappings/visual-mode"),
-}
--- .config/nvim/lua/mappings/insert-mode.lua
+++
@@ -1,2 +0,0 @@
--- NERDTree
-vim.keymap.set("i", "jk", "<ESC>", {noremap=true})
