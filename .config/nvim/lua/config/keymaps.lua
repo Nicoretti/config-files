1baf551f9fb0ea70a0076d010dd59ba20c21a5c9
Nicola Coretti <nico.coretti@gmail.com>
2024-02-03 21:40:47 +0100

    Add description to nvim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = ","
 -- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Normal Mode >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
 -- Convert to hex view
-vim.keymap.set("n", "<leader>X", ":%!xxd<CR>", {noremap=true})
+vim.keymap.set("n", "<leader>X", ":%!xxd<CR>", {noremap=true, desc="Switch to Hex-Mode"})
 -- Convert to normal view
-vim.keymap.set("n", "<leader>x", ":%!xxd -r<CR>", {noremap=true})
+vim.keymap.set("n", "<leader>x", ":%!xxd -r<CR>", {noremap=true, desc="Switch to Text-Mode"})
 -- toggle binary mode
 vim.keymap.set("n", "<leader>b", ":set binary!<CR>", {noremap=true})
 
